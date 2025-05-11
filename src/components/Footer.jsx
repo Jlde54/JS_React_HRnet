e78de29a02c393b980d32ff0c4ea1a874bd57b48
Jlde54 <jlde54@sfr.fr>
2025-05-11 16:43:20 +0200

    Footer.jsx : added some comments Header.jsx : added logo Wealth Health + some comments Header.modulke.scss : updated styles for logo Wealth Health performance-reports : updated reports for jQuery version published on GitHub pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,12 @@
 import styles from '../styles/Footer.module.scss'
 
-const Footer = () => {
+/**
+ * Component displaying the footer.
+ *
+ * @component
+ * @returns {JSX.Element}
+ */
+function Footer () {
   return (
     <footer className={styles.footer}>
       <p className={styles.footer__text}>© 2025 HRnet. All rights reserved.</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,30 @@
 import { Link, useLocation } from 'react-router-dom'
 import styles from '../styles/Header.module.scss'
+import logo from '../assets/WealthHealth.png'
 
+/**
+ * Component displaying the header.
+ *
+ * @component
+ * @returns {JSX.Element}
+ */
 function Header () {
+
   const location = useLocation()
   const isOnCreateEmployee = location.pathname === '/'
   const isOnEmployeeList = location.pathname === '/Employees'
+
   return (
     <header className={styles.header}>
       <div className={styles.header__container}>
-        <h1 className={styles.header__logo}>HRnet</h1>
+        <img src={logo} alt="HRNet Logo" className={styles.header__logo} />
+        <h1 className={styles.header__title}>HRnet</h1>
         <nav className={styles.header__nav}>
           {isOnEmployeeList && (
             <Link to='/' className={styles.header__link}>Home</Link>
           )}
           {isOnCreateEmployee && (
-            <Link to='/Employees' className={styles.header__link}>View current employees</Link>
+            <Link to='/Employees' className={styles.header__link}>Current employees</Link>
           )}
         </nav>
       </div>
